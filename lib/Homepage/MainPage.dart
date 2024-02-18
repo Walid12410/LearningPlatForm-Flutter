3fa0a8efb99efb9e3f5ea00be54796c557af0643
walid jallad <78869109+Walid12410@users.noreply.github.com>
2024-02-19 01:51:23 +0200

    upgrade homepage

--- lib/Homepage/MainPage.dart
+++ lib/Homepage/MainPage.dart
@@ -3,12 +3,11 @@ import 'package:learningplatformapp/AllClass/trainer.dart';
 import 'package:learningplatformapp/AllClass/course.dart';
 import 'package:learningplatformapp/Homepage/widget/RecommendedCourse.dart';
 import 'widget/dialogpage.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'package:learningplatformapp/colors/color.dart';
 import 'widget/specialforyou.dart';
 import 'widget/courseviwe.dart';
 import 'widget/LatestCourseAdded.dart';
+import 'dart:math';
 
 class MainPage extends StatefulWidget {
   const MainPage({required this.userid, Key? key}) : super(key: key);
@@ -24,6 +23,7 @@ class _MainPageState extends State<MainPage> {
   bool _isLoadingTrainers = false;
   bool _isLoadingCourseViews = false;
   bool _isLoadingLatestCourses = false;
+  bool _isLoadingRecommendedCourses = false;
 
   @override
   void initState() {
@@ -33,9 +33,20 @@ class _MainPageState extends State<MainPage> {
 
   void _fetchData() {
     _fetchTrainers();
+    _fetchRecommendedCourses();
     getCourseView();
     getCourseNew();
     getAllCourses();
+  }
+
+  void _fetchRecommendedCourses() async {
+    setState(() {
+      _isLoadingRecommendedCourses = true; // Set loading state to true
+    });
+    await Future.delayed(Duration(seconds: 2)); // Simulating delay
+    setState(() {
+      _isLoadingRecommendedCourses = false;
+    });
   }
 
   void _fetchTrainers() async {
@@ -135,7 +146,7 @@ class _MainPageState extends State<MainPage> {
                 ),
               ),
               _isLoadingCourseViews
-                  ? CircularProgressIndicator()
+                  ? const CircularProgressIndicator()
                   : Specialforyou(text: 'Most Viewed Courses', press: () {}),
               const SizedBox(height: 5),
               _isLoadingCourseViews
@@ -146,39 +157,34 @@ class _MainPageState extends State<MainPage> {
                         padding: const EdgeInsets.all(1),
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            if (courseviews.isNotEmpty)
-                              CourseView(
-                                cname: courseviews[0].name,
-                                image: 'assets/courseview/image1.png',
-                                price: courseviews[0].price,
-                                view: courseviews[0].view,
-                                press: () {},
-                              ),
-                            if (courseviews.length > 1)
-                              CourseView(
-                                cname: courseviews[1].name,
-                                image: 'assets/courseview/image2.png',
-                                price: courseviews[1].price,
-                                view: courseviews[1].view,
-                                press: () {},
-                              ),
-                            if (courseviews.length > 2)
-                              CourseView(
-                                cname: courseviews[2].name,
-                                image: 'assets/courseview/image3.png',
-                                price: courseviews[2].price,
-                                view: courseviews[2].view,
-                                press: () {},
-                              ),
-                            const SizedBox(width: 10),
-                          ],
+                          children: _isLoadingCourseViews
+                              ? [const CircularProgressIndicator()]
+                              : List.generate(
+                                  min(
+                                      3,
+                                      courseviews
+                                          .length), // Utilize max function to ensure at least 3 items are displayed
+                                  (index) {
+                                    if (index < courseviews.length) {
+                                      return CourseView(
+                                        cname: courseviews[index].name,
+                                        image:
+                                            'assets/courseview/image${index + 1}.png',
+                                        price: courseviews[index].price,
+                                        view: courseviews[index].view,
+                                        press: () {},
+                                      );
+                                    } else {
+                                      return const SizedBox(); // Return empty SizedBox if index is out of bounds
+                                    }
+                                  },
+                                ),
                         ),
                       ),
                     ),
               const SizedBox(height: 5),
               _isLoadingLatestCourses
-                  ? CircularProgressIndicator()
+                  ? const CircularProgressIndicator()
                   : Specialforyou(text: 'Latest Course Added', press: () {}),
               const SizedBox(height: 5),
               _isLoadingLatestCourses
@@ -188,32 +194,26 @@ class _MainPageState extends State<MainPage> {
                       child: Padding(
                         padding: const EdgeInsets.all(1),
                         child: Row(
-                          children: [
-                            if (courseadd.isNotEmpty)
-                              LatestCourseAdd(
-                                image: 'assets/latestadd/image32.png',
-                                price: courseadd[0].price,
-                                name: courseadd[0].name,
-                              ),
-                            if (courseadd.length > 1)
-                              LatestCourseAdd(
-                                image: 'assets/latestadd/image12.png',
-                                price: courseadd[1].price,
-                                name: courseadd[1].name,
-                              ),
-                            if (courseadd.length > 2)
-                              LatestCourseAdd(
-                                image: 'assets/latestadd/image22.png',
-                                price: courseadd[2].price,
-                                name: courseadd[2].name,
-                              ),
-                            if (courseadd.length > 3)
-                              LatestCourseAdd(
-                                image: 'assets/latestadd/image42.png',
-                                price: courseadd[3].price,
-                                name: courseadd[3].name,
-                              ),
-                          ],
+                          children: _isLoadingLatestCourses
+                              ? [const CircularProgressIndicator()]
+                              : List.generate(
+                                  min(
+                                      4,
+                                      courseadd
+                                          .length), // Ensure at least 4 items are displayed
+                                  (index) {
+                                    if (index < courseadd.length) {
+                                      return LatestCourseAdd(
+                                        image:
+                                            'assets/latestadd/image${index + 1}.png',
+                                        price: courseadd[index].price,
+                                        name: courseadd[index].name,
+                                      );
+                                    } else {
+                                      return const SizedBox(); // Return empty SizedBox if index is out of bounds
+                                    }
+                                  },
+                                ),
                         ),
                       ),
                     ),
@@ -221,39 +221,32 @@ class _MainPageState extends State<MainPage> {
               Specialforyou(text: 'Recommended Course', press: () {}),
               const SizedBox(height: 5),
               SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Padding(
-                    padding: const EdgeInsets.all(1.0),
-                    child: Row(
-                      children: [
-                        RecommendedCourse(
-                            cname: allCourses[0].name,
-                            image: 'assets/rc/i1.png',
-                            price: allCourses[0].price,
-                            press: () {}),
-                        RecommendedCourse(
-                            cname: allCourses[1].name,
-                            image: 'assets/rc/i2.png',
-                            price: allCourses[1].price,
-                            press: () {}),
-                        RecommendedCourse(
-                            cname: allCourses[2].name,
-                            image: 'assets/rc/i3.png',
-                            price: allCourses[2].price,
-                            press: () {}),
-                        RecommendedCourse(
-                            cname: allCourses[3].name,
-                            image: 'assets/rc/i4.png',
-                            price: allCourses[3].price,
-                            press: () {}),
-                        RecommendedCourse(
-                            cname: allCourses[4].name,
-                            image: 'assets/rc/i5.png',
-                            price: allCourses[4].price,
-                            press: () {}),
-                      ],
-                    ),
-                  )),
+                scrollDirection: Axis.horizontal,
+                child: Padding(
+                  padding: const EdgeInsets.all(1.0),
+                  child: Row(
+                    children: _isLoadingRecommendedCourses // Check if loading
+                        ? [
+                            const CircularProgressIndicator()
+                          ] // Display loading indicator
+                        : List.generate(
+                            // Use List.generate to create a list of recommended courses or loading indicator
+                            min(
+                                5,
+                                allCourses
+                                    .length), // Limit to a maximum of 5 times
+                            (index) {
+                              return RecommendedCourse(
+                                cname: allCourses[index].name,
+                                image: 'assets/rc/i${index + 1}.png',
+                                price: allCourses[index].price,
+                                press: () {},
+                              );
+                            },
+                          ),
+                  ),
+                ),
+              ),
               const SizedBox(
                 height: 10,
               ),
