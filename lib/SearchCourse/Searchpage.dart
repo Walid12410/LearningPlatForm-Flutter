bb0039c74817a649ab91a77bc936d809da9efeed
walid jallad <78869109+Walid12410@users.noreply.github.com>
2024-03-01 23:22:15 +0200

    SearchPageFinishWithFilter

--- lib/SearchCourse/Searchpage.dart
+++ lib/SearchCourse/Searchpage.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:learningplatformapp/futureapi/CourseApi.dart';
+import 'package:provider/provider.dart';
 import 'package:learningplatformapp/colors/color.dart';
+import 'package:learningplatformapp/provider/provider_data.dart';
+import 'package:learningplatformapp/AllClass/course.dart';
+import 'package:learningplatformapp/AllClass/portal.dart';
 
 class SearchPage extends StatefulWidget {
   @override
@@ -9,11 +14,24 @@ class SearchPage extends StatefulWidget {
 class _SearchPageState extends State<SearchPage> {
   TextEditingController _textFieldController = TextEditingController();
   FocusNode _focusNode = FocusNode();
+  List<Course> allCourses = [];
+  List<Course> filteredCourses = [];
+  String selectedCategory = '';
+
+  double minPrice = 0.0;
+  double maxPrice = 5000.0;
 
   @override
   void initState() {
     super.initState();
     _focusNode.requestFocus();
+    getAllCourses(context).then((_) {
+      setState(() {
+        allCourses =
+            Provider.of<AppDataProvider>(context, listen: false).allCourses;
+        filteredCourses = allCourses;
+      });
+    });
   }
 
   @override
@@ -22,49 +40,291 @@ class _SearchPageState extends State<SearchPage> {
     super.dispose();
   }
 
+  void filterCourses(String query, String selectedCategory) {
+    setState(() {
+      filteredCourses = allCourses
+          .where((course) =>
+              course.name.toLowerCase().contains(query.toLowerCase()) &&
+              (selectedCategory.isEmpty ||
+                  course.portalID.toString() == selectedCategory))
+          .toList();
+    });
+  }
+
+  Future<void> _showPriceDialog(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return Consumer<AppDataProvider>(
+          builder: (context, appDataProvider, _) {
+            List<Portal>? portals = appDataProvider.portals;
+            print('Portals length: ${portals?.length}');
+            print('Portals content: $portals');
+            if (portals == null || portals.isEmpty) {
+              return AlertDialog(
+                title: const Text('No Category found'),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('OK'),
+                  ),
+                ],
+              );
+            }
+            Set<String> uniqueValues = {};
+            return AlertDialog(
+              title: const Text(
+                'Quick Filters',
+                style: TextStyle(color: tdbrown, fontWeight: FontWeight.bold),
+              ),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  DropdownButtonFormField<String>(
+                    decoration: InputDecoration(
+                      labelStyle: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                        color: tdBlue,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: const BorderSide(
+                          color: tdBlue,
+                          width: 1,
+                        ),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: const BorderSide(
+                          color: Colors.grey,
+                          width: 1,
+                        ),
+                      ),
+                    ),
+                    value: selectedCategory,
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        selectedCategory = newValue ?? ''; // Ensure newValue is not null
+                      });
+                    },
+                    items: [
+                      const DropdownMenuItem<String>(
+                        value: '',
+                        child: Text('All Category Selected'),
+                      ),
+                      ...portals.map<DropdownMenuItem<String>>((Portal portal) {
+                        if (!uniqueValues.contains(portal.portalID.toString())) {
+                          uniqueValues.add(portal.portalID.toString());
+                          return DropdownMenuItem<String>(
+                            value: portal.portalID.toString(),
+                            child: Text(portal.portalName),
+                          );
+                        } else {
+                          return DropdownMenuItem<String>(value: '', child: SizedBox.shrink());
+                        }
+                      }).whereType<DropdownMenuItem<String>>().toList(), // Filter out null values
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: TextField(
+                            controller: TextEditingController(text: minPrice.toString()),
+                            onChanged: (value) {
+                              setState(() {
+                                minPrice = double.tryParse(value) ?? 0.0;
+                              });
+                            },
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              labelText: 'Category',
+                              labelStyle: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                                color: tdBlue,
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15),
+                                borderSide: const BorderSide(
+                                  color: tdBlue,
+                                  width: 1,
+                                ),
+                              ),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15),
+                                borderSide: const BorderSide(
+                                  color: Colors.grey,
+                                  width: 1,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 8.0),
+                          child: TextField(
+                            controller: TextEditingController(text: maxPrice.toString()),
+                            onChanged: (value) {
+                              setState(() {
+                                maxPrice = double.tryParse(value) ?? 5000.0;
+                              });
+                            },
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              labelText: 'Maximum Price',
+                              labelStyle: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                                color: tdBlue,
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15),
+                                borderSide: const BorderSide(
+                                  color: tdBlue,
+                                  width: 1,
+                                ),
+                              ),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15),
+                                borderSide: const BorderSide(
+                                  color: Colors.grey,
+                                  width: 1,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Cancel'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    // Apply filter logic
+                    setState(() {
+                      filteredCourses = allCourses.where((course) {
+                        if (minPrice != null && course.price < minPrice!) {
+                          return false;
+                        }
+                        if (maxPrice != null && course.price > maxPrice!) {
+                          return false;
+                        }
+                        if (selectedCategory.isNotEmpty &&
+                            course.portalID.toString() != selectedCategory) {
+                          return false;
+                        }
+                        return true;
+                      }).toList();
+                    });
+
+                    // Close the dialog
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Done'),
+                )
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Column(
           children: [
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: double.infinity,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: tdbrown,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: TextField(
-                    focusNode: _focusNode,
-                    controller: _textFieldController,
-                    cursorColor: tdBlue,
-                    decoration:const InputDecoration(
-                      hintText: 'Search',
-                      border: InputBorder.none,
-                      prefixIcon: Icon(Icons.search, color: tdBlue),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: tdbrown,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: TextField(
+                        focusNode: _focusNode,
+                        controller: _textFieldController,
+                        cursorColor: tdBlue,
+                        onChanged: (query) {
+                          filterCourses(query, selectedCategory);
+                        },
+                        decoration: const InputDecoration(
+                          hintText: 'Search',
+                          border: InputBorder.none,
+                          prefixIcon: Icon(Icons.search, color: tdBlue),
+                        ),
+                        style: const TextStyle(color: tdBlue),
+                      ),
                     ),
-                    style:const TextStyle(color: tdBlue),
                   ),
-                ),
+                  IconButton(
+                    icon: const Icon(Icons.filter_list, color: tdbrown),
+                    onPressed: () {
+                      _showPriceDialog(context);
+                    },
+                  ),
+                ],
               ),
             ),
-            InkWell(
-              onTap: () {},
-              child:const Icon(
-                Icons.display_settings,
-                size: 50,
-                color: tdbrown,
-              ),
-            )
+            Expanded(
+              child: filteredCourses.isEmpty
+                  ? const Center(child: Text('Course not found'))
+                  : ListView.builder(
+                      itemCount: filteredCourses.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5), color: tdbrown),
+                              child: ListTile(
+                                title: Text(
+                                  filteredCourses[index].name,
+                                  style:
+                                  const TextStyle(color: tdBlue, fontWeight: FontWeight.bold),
+                                ),
+                                subtitle: Text(
+                                  filteredCourses[index].description,
+                                  style: const TextStyle(color: tdBlue),
+                                ),
+                                trailing: Text(
+                                  '\$${filteredCourses[index].price}',
+                                  style: const TextStyle(fontSize: 15, color: tdBlue),
+                                ),
+                                onTap: () {
+                                },
+                              )
+                          ),
+                        );
+                      },
+                    ),
+            ),
           ],
         ),
       ),
     );
   }
 }
-
